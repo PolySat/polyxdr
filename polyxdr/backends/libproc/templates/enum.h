56e45e94db5652a803b69bcddba19dfa14ada2cf
jograce <joshgrace15@gmail.com>
2019-11-21 19:19:46 -0800

    reverted enum.h

--- polyxdr/backends/libproc/templates/enum.h
+++ polyxdr/backends/libproc/templates/enum.h
@@ -1,7 +1,6 @@
-struct XDR_EnumMapping ${enum.name.replace('::','_',400)} [] = {
+enum ${enum.name.replace('::','_',400)} {
 :: for m in enum.members:
-   { ${str(m.value).replace('::','_',400)}, "${m.name.replace('::','_',400)}" },
+   ${m.name.replace('::','_',400)} = ${str(m.value).replace('::','_',400)},
 :: #endfor
-   { 0, NULL }
 };
 
